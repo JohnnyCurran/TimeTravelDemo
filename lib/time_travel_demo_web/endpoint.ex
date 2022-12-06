003a90d69f79c3bb6b256ce4a41390d939f98145
John Curran <john@talentwall.io>
2022-12-06 15:41:39 -0600

    Socket configuration

--- lib/time_travel_demo_web/endpoint.ex
+++ lib/time_travel_demo_web/endpoint.ex
@@ -12,6 +12,10 @@ defmodule TimeTravelDemoWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  socket "/socket", TimeTravelDemoWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
