1fead144567f28c28c432a524cb12077cfae9728
John Curran <john@talentwall.io>
2022-12-10 11:40:57 -0600

    Use a word list sigil

--- lib/time_travel_demo_web/live/page_live.ex
+++ lib/time_travel_demo_web/live/page_live.ex
@@ -1,15 +1,15 @@
 defmodule TimeTravelDemoWeb.PageLive do
   use TimeTravelDemoWeb, :live_view
 
-  @list_ipsum ["Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing",
- "elit,", "sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et",
- "dolore", "magna", "aliqua.", "Ut", "enim", "ad", "minim", "veniam,", "quis",
- "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex",
- "ea", "commodo", "consequat.", "Duis", "aute", "irure", "dolor", "in",
- "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu",
- "fugiat", "nulla", "pariatur.", "Excepteur", "sint", "occaecat", "cupidatat",
- "non", "proident,", "sunt", "in", "culpa", "qui", "officia", "deserunt",
- "mollit", "anim", "id", "est", "laborum."]
+  @list_ipsum ~w(Lorem, ipsum, dolor, sit, amet,, consectetur, adipiscing,
+    elit,, sed, do, eiusmod, tempor, incididunt, ut, labore, et,
+    dolore, magna, aliqua., Ut, enim, ad, minim, veniam,, quis,
+    nostrud, exercitation, ullamco, laboris, nisi, ut, aliquip, ex,
+    ea, commodo, consequat., Duis, aute, irure, dolor, in,
+    reprehenderit, in, voluptate, velit, esse, cillum, dolore, eu,
+    fugiat, nulla, pariatur., Excepteur, sint, occaecat, cupidatat,
+    non, proident,, sunt, in, culpa, qui, officia, deserunt,
+    mollit, anim, id, est, laborum.)
 
   @impl true
   def mount(_params, _session, socket) do
