2d422be7a1d478b5cd273b0e0df04cf2d4cb84e7
John Curran <john@talentwall.io>
2022-12-12 10:25:57 -0600

    Add handle_params callback, back to path dependencies

--- lib/time_travel_demo_web/live/page_live.ex
+++ lib/time_travel_demo_web/live/page_live.ex
@@ -1,19 +1,16 @@
 defmodule TimeTravelDemoWeb.PageLive do
   use TimeTravelDemoWeb, :live_view
 
-  @list_ipsum ~w(Lorem ipsum dolor sit amet consectetur adipiscing
-    elit sed do eiusmod tempor incididunt ut labore et
-    dolore magna aliqua. Ut enim ad minim veniam quis
-    nostrud exercitation ullamco laboris nisi ut aliquip ex
-    ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu
-    fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident sunt in culpa qui officia deserunt
-    mollit anim id est laborum.)
+  @list_ipsum ~w(Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum.)
 
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, :items, [])}
+  end
+
+  @impl true
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
   end
 
   @impl true
